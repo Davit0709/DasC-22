be0559a371ac83e2f263eff8f5ae1e6c2ef17cc7
Davit <dav0709@icloud.com>
2023-04-04 20:50:58 +0400

    Uxagrum

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,,] array = new int[2, 2, 2];
+        int count = 10;
+        for (int x = 0; x < 2; x++)
+        {
+            for (int y = 0; y < 2; y++)
+            {
+                for (int z = 0; z < 2; z++)
+                {
+                    array[x, y, z] = count;
+                    count++;
+                }
+            }
+        }
+
+        int rowNum = 0;
+
+        for (int x = 0; x < 2; x++)
+        {
+            for (int y = 0; y < 2; y++)
+            {
+                for (int z = 0; z < 2; z++)
+                {
+                    Console.Write(array[x, y, z].ToString().PadLeft(2, '0'));
+                    Console.Write("(" + x + "," + y + "," + z + ") ");
+
+
+                    if (z == 1)
+                    {
+                        rowNum++;
+                        Console.WriteLine("Row " + rowNum);
+                    }
+                }
+            }
+        }
+    }
+}
